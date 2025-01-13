106a135c44ce55ed383fa720a7042decb1910fcc
dragomir <dragomir.sergeev@yandex.ru>
2025-01-13 17:24:28 +0300

    hw01 конфигурирование spring через xml

---
+++ src/main/java/ru/otus/hw/config/TestFileProviderImpl.java
@@ -0,0 +1,16 @@
+package ru.otus.hw.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class TestFileProviderImpl implements TestFileNameProvider {
+
+    private String testFileName;
+
+}
--- src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,16 +1,33 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
+    private static final String QUESTION_SEPARATOR = "__________________________";
+
     private final IOService ioService;
+
+    private final QuestionDao questionDao;
 
     @Override
     public void executeTest() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
         // Получить вопросы из дао и вывести их с вариантами ответов
+        List<Question> questions = questionDao.findAll();
+
+        for (Question question : questions) {
+            ioService.printLine("Question: " + question.text());
+            AtomicInteger questionCounter = new AtomicInteger(1);
+            question.answers().forEach(a -> ioService.printLine("Answer " + questionCounter.getAndIncrement() + ": " + a.text()));
+            ioService.printLine(QUESTION_SEPARATOR);
+        }
     }
 }
